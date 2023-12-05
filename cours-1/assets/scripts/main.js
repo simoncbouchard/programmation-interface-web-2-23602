f5b24b96a154153b51eb95f308db454146855721
simoncbouchard <simoncbouchard@gmail.com>
2023-12-05 10:09:58 -0500

    Add cours-1

---
+++ cours-1/assets/scripts/main.js
@@ -0,0 +1,11 @@
+/**
+ * Vous avez une structure de data listant six états (fichier oEtats.js), vous devez :
+ *      - injecter une tuile pour chacune des six villes au chargement de la page
+ *      - injecter les informations associées à cette ville au survol d'une tuile
+ *      - supprimer le noeud DOM précédement injecté à la sortie du curseur d'une tuile
+ *      - faire la gestion des transitions d'arrivée et de sortie
+ *      - notez que toutes les propriétés CSS nécessaires aux comportements demandés sont déjà définies
+ */
+
+
+
---
+++ cours-1/assets/scripts/oEtats.js
@@ -0,0 +1,50 @@
+const oEtats = {
+  'France': {
+    capitale: 'Paris',
+    population: '67&thinsp;076&thinsp;000',
+    superficie: '551&thinsp;695',
+    langue: 'Français',
+    monnaie: 'Euro',
+    img: 'france'
+  },
+  'Japon': {
+    capitale: 'Tokyo',
+    population: '126&thinsp;800&thinsp;000',
+    superficie: '377&thinsp;975',
+    langue: 'Japonais',
+    monnaie: 'Yen',
+    img: 'japon'
+  },
+  'Brésil': {
+    capitale: 'Brasilia',
+    population: '212&thinsp;600&thinsp;000',
+    superficie: '8&thinsp;515&thinsp;767',
+    langue: 'Portugais',
+    monnaie: 'Real',
+    img: 'bresil'
+  },
+  'États-Unis': {
+    capitale: 'Washington, D.C.',
+    population: '328&thinsp;200&thinsp;000',
+    superficie: '9&thinsp;833&thinsp;520',
+    langue: 'Anglais',
+    monnaie: 'Dollar américain',
+    img: 'etats-unis'
+  },
+  'Inde': {
+    capitale: 'New Delhi',
+    population: '1&thinsp;339&thinsp;000&thinsp;000',
+    superficie: '3&thinsp;287&thinsp;263',
+    langue: 'Hindi',
+    monnaie: 'Roupie indienne',
+    img: 'inde'
+  },
+  'Australie': {
+    capitale: 'Canberra',
+    population: '25&thinsp;650&thinsp;000',
+    superficie: '7&thinsp;692&thinsp;024',
+    langue: 'Anglais',
+    monnaie: 'Dollar australien',
+    img: 'australie'
+  }
+}
