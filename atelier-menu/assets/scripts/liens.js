8b60479c0c934d5ca66130a56d671dbf52d629ff
simoncbouchard <simoncbouchard@gmail.com>
2024-01-25 09:00:23 -0500

    Add atelier-menu

---
+++ atelier-menu/assets/scripts/liens.js
@@ -0,0 +1,37 @@
+const liens = [
+    {
+        site: 'MDN',
+        url: 'https://developer.mozilla.org/fr/',
+        externe: true
+    },
+    {
+        site: 'JavaScript.info',
+        url: 'https://fr.javascript.info/',
+        externe: true
+    },
+    {
+        site: 'Pierre Giraud',
+        url: 'https://www.pierre-giraud.com/javascript-apprendre-coder-cours/',
+        externe: true
+    },
+    {
+        site: 'Gekkode',
+        url: 'https://www.gekkode.com/category/developpement/javascript/',
+        externe: true
+    },
+    {
+        site: 'Codecademy',
+        url: 'https://www.codecademy.com/learn/introduction-to-javascript',
+        externe: true
+    },
+    {
+        site: 'JavaScript Tutorial',
+        url: 'https://www.javascripttutorial.net/',
+        externe: true
+    },
+    {
+        site: 'DevDocs JavaScript',
+        url: 'https://devdocs.io/javascript/',
+        externe: true
+    }
+];
---
+++ atelier-menu/assets/scripts/main.js
@@ -0,0 +1,6 @@
+/**
+ * - Faire la gestion des états d'affichage (fermé / ouvert) du menu
+ * - Prévenir le scroll en Y losque le menu est ouvert
+ * - Ajouter des transitions d'arrivée et de sortie
+ * - Populer le menu à l'aide de la structure de données (liens.js), chaque lien est un composant modulaire
+ */
