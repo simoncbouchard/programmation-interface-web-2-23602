3617095060262fcc1c029a552cd9ab8bff6b4c23
simoncbouchard <simoncbouchard@gmail.com>
2024-02-08 07:09:25 -0500

    Add cours-10-exercice

---
+++ cours-10-exercice/assets/scripts/main.js
@@ -0,0 +1,17 @@
+import { classesMapping } from './classMapping.js';
+
+
+(function() {
+
+	let elsComponent = document.querySelectorAll('[data-js-component]');
+	
+	for (let i = 0, l = elsComponent.length; i < l; i++) {
+		
+		let component = elsComponent[i].dataset.jsComponent,				// => string
+			elComponent = elsComponent[i];
+
+		for (let cle in classesMapping) {
+			if (component == cle) new classesMapping[component](elComponent);
+		}
+	}
+})();
