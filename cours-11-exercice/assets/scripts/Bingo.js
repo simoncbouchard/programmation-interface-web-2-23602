892247da46f139541ff098e1d798284538d571bf
simoncbouchard <simoncbouchard@gmail.com>
2024-02-12 11:22:11 -0500

    Add cours-11-exercice

---
+++ cours-11-exercice/assets/scripts/Bingo.js
@@ -0,0 +1,11 @@
+/**
+ * Du registre québécois des données publiques (https://www.donneesquebec.ca/recherche/dataset/?sort=metadata_created+desc&res_format=JSON),
+ * injectez de façon asynchrone dans l'élément <select> les villes titulaires de bingo(s) de la catégorie 'MEDIA'
+ * puis, à l'événement change du <select>, affichez les titulaires de licence de bingo de la ville sélectionnée. 
+ * 
+ * Fiche descriptive de la ressource : https://www.donneesquebec.ca/recherche/dataset/racj-licence-bingo/resource/d29b1267-ad6d-4ee7-a4e4-86fe1163d9fb
+ * Licences de bingo en format JSON : https://www.donneesquebec.ca/recherche/dataset/2180052d-8382-4938-ae14-37f5e594d26f/resource/d29b1267-ad6d-4ee7-a4e4-86fe1163d9fb/download/racj-licence-bingo.json
+ */
+
+
+// class Bingo 
